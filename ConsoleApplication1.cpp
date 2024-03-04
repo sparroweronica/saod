ca76f0783f78a16e103f9692c4b08e7ca08a58b5
sparroweronica <159920604+sparroweronica@users.noreply.github.com>
2024-03-05 01:33:11 +0300

    2

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a[5]{1,2,3,4,5};
+    for (int i = 0; i < 5; i++)
+        cout << a[i] << ' ';
+    cout << endl;
+
+    for (int* p = a; p - a < 5; p++)
+        cout << *p << ' ';
+    cout << endl;
+    return 0;
+}
+
