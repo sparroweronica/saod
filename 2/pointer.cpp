553ea54d7932dddc44feff87a112b86b79c693cc
sparroweronica <sparroweronica@gmail.com>
2024-03-05 15:32:22 +0300

    ю

---
+++ 2/pointer.cpp
@@ -0,0 +1,13 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 3;
+    int* p = &x;
+    cout << x << ' ' << *p << ' ' << p << endl;
+    p++;
+    cout << p << endl;
+    cout << p - &x << endl;
+}
