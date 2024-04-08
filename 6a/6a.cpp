52750c720be454c577ee636d7bf178fcbefa3fb8
sparroweronica <sparroweronica@gmail.com>
2024-04-09 00:57:46 +0300

    5 вариант

---
+++ 6a/6a.cpp
@@ -0,0 +1,47 @@
+﻿
+#include <iostream>
+#include <string>
+
+class MyString {
+private:
+    std::string str;
+
+public:
+   
+    MyString(int x) {
+        const int buffSize = 20; 
+        char buff[buffSize];
+        int result = _itoa_s(x, buff, buffSize, 10); 
+        if (result != 0) {
+            std::cerr << "Ошибка при конвертации!" << std::endl;
+    
+        }
+        else {
+            str = buff; 
+        }
+    }
+
+
+    std::string substr(int off, int len) const {
+
+        if (off >= str.length()) {
+            return ""; 
+        }
+        len = std::min(len, static_cast<int>(str.length()) - off);
+
+        return str.substr(off, len);
+    }
+
+    void print() const {
+        std::cout << str << std::endl;
+    }
+};
+
+int main() {
+    MyString str(123456789); 
+    str.print(); 
+    std::string sub = str.substr(2, 5); 
+    std::cout << "подстрока: " << sub << std::endl; 
+
+    return 0;
+}
